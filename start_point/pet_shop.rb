0197734f1c951c313eb112a92bf37b28b06449da
Paul Wishart <misterwishart@gmail.com>
2018-10-19 13:45:01 +0100

    Completed Test 12

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -41,3 +41,11 @@ def find_pet_by_name(shop, petname)
   #404: Pet not found!
   return nil
 end
+
+def remove_pet_by_name(shop, petname)
+  for pet in shop[:pets]
+    if pet[:name] == petname
+      shop[:pets].delete(pet)
+    end
+  end
+end
